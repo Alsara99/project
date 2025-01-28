8b78a39a59fe7d3f61de7d20a91bbf5f387d52bf
Никита Станьков <9alsara9@gmail.com>
2025-01-28 21:32:36 +0300

    фиксация прогресса по прошлой домашке

---
+++ src/masks.py
@@ -0,0 +1,19 @@
+def get_mask_card_number(number: int) -> str:
+    """Возвращает замаскированный номер карты"""
+    card_number = str(number)
+    groups_of_card_number = [card_number[:4], card_number[4:6] + "**", "****", card_number[-4:]]
+    result = ""
+    for i in groups_of_card_number:
+        result = result + i + " "
+    return result
+
+
+def get_mask_account(number: int) -> str:
+    """Возвращает замаскированный номер счёта"""
+    account_number = str(number)
+    result = "**" + account_number[-4:]
+    return result
+
+
+print(get_mask_card_number(7000792289606361))
+print(get_mask_account(73654108430135874305))
