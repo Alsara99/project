77f371f23e48b6b1c3cee131a00b8c26f61967a8
Alsara99 <9alsara9@gmail.com>
2025-03-25 12:42:21 +0300

    Update file_helper.py

--- src/file_helper.py
+++ src/file_helper.py
@@ -2,12 +2,14 @@ import pandas as pd
 
 
 def read_csv_file(path):
+    """происходит считывание финансовых операций из csv-файла"""
     data = pd.read_csv(path, encoding='UTF-8', delimiter=';')
     transactions = data.to_dict(orient='records')
     return transactions
 
 
 def read_excel_file(path):
+    """происходит считывание финансовых операция их xlsx-файла"""
     data = pd.read_excel(path)
     transactions = data.to_dict(orient='records')
     return transactions
