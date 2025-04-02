b4480ef0cdfade3f1dda035709470ca95c95b864
Alsara99 <9alsara9@gmail.com>
2025-04-03 00:18:16 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import json
 import logging
 
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler('../PythonProject/logs/utils.log')
+file_handler = logging.FileHandler('..logs/utils.log')
 file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
 
 file_handler.setFormatter(file_formatter)
